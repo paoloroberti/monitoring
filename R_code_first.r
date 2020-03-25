5c73b5738c8374ff6d6402c71f88bf732a823fc7
paoloroberti <62661886+paoloroberti@users.noreply.github.com>
2020-03-25 18:05:56 +0100

    Create R_code_first.r

---
+++ R_code_first.r
@@ -0,0 +1,18 @@
+install.packages("sp")
+
+library(sp)
+data(meuse)
+
+# let's see how the meuse dataset is structured:
+meuse
+
+# let's look at the first row of the set 
+head(meuse)
+
+#let's plot two variables
+#let's see if the zin concentration is related to that of copper
+attach(meuse)
+plot(zinc,copper)
+plot(zinc,copper,col="green")
+plot(zinc,copper,col="green",pch=19)
+plot(zinc,copper,col="green",pch=19,cex=2)
