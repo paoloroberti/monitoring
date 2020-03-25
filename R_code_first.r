8e99a6b1032c76c3b505353b5c8e73048702fedc
Paolo Roberti <62661886+paoloroberti@users.noreply.github.com>
2020-03-25 18:57:40 +0100

    Aggiunto commento test

--- R_code_first.r
+++ R_code_first.r
@@ -6,7 +6,7 @@ data(meuse)
 # let's see how the meuse dataset is structured:
 meuse
 
-# let's look at the first row of the set 
+# let's look at the first row of the set
 head(meuse)
 
 #let's plot two variables
@@ -16,3 +16,5 @@ plot(zinc,copper)
 plot(zinc,copper,col="green")
 plot(zinc,copper,col="green",pch=19)
 plot(zinc,copper,col="green",pch=19,cex=2)
+
+#test
