c247fc6c6eb386556e904f605b4e82e882fa2fc4
Paolo Roberti <62661886+paoloroberti@users.noreply.github.com>
2020-03-27 18:28:12 +0100

    variation

--- R_code_multipanel.r
+++ R_code_multipanel.r
@@ -1,9 +1,11 @@
 ### Multipanel in R: the second lecture of monitoring ecosystems
 
 install.packages("sp")
+install.packages("GGally",dependencies=T) #intallation of new packages
+
 
 library(sp) #require(sp) will also do teh same job, telling R that we are going to use this package
-
+library("GGally")
 data(meuse) #data means there is a dataset available named meuse
 
 attach(meuse)
@@ -18,4 +20,17 @@ plot(cadmium,zinc,pch=15,col="red",cex=3)
 #exercise:make all possible pairwis plots of dataset
 #use of plo() is not so good couse it's too long plot each pair
 
-pairs(meuse)
+pairs(meuse) # you can plot all the variables together
+
+#prettify the plot, reduce variables
+pairs(~cadmium+copper+lead+zinc,data=meuse)
+
+pairs(meuse[,3:6]) # [] subset of a dataset, comma means "start from". this produce the same result of the prevoius function
+
+#exercise: prettify this graph
+pairs(meuse[,3:6],pch=13,col="green")
+pairs(meuse[,3:6],pch=18,col="green",cex=1.5)
+
+#after this point something goes wrong--> install and library of GGally
+
+ggpairs(meuse[,3:6])#function after install and library of GGally
