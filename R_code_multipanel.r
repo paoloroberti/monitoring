c3a126574bedef006845d085e3025d0b05a5533f
paoloroberti <62661886+paoloroberti@users.noreply.github.com>
2020-03-28 09:10:21 +0100

    Update R_code_multipanel.r

--- R_code_multipanel.r
+++ R_code_multipanel.r
@@ -18,7 +18,7 @@ head(meuse)
 plot(cadmium,zinc,pch=15,col="red",cex=3)
 
 #exercise:make all possible pairwis plots of dataset
-#use of plo() is not so good couse it's too long plot each pair
+#use of plot() is not so good couse it's too long plot each pair
 
 pairs(meuse) # you can plot all the variables together
 
