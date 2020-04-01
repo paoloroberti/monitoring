c4a69d5bf99ada1d1d7d54f13239edfbadde1b68
Paolo Roberti <62661886+paoloroberti@users.noreply.github.com>
2020-04-01 16:57:23 +0200

    new

--- R_code_spatial.r
+++ R_code_spatial.r
@@ -1,2 +1,19 @@
 # R code for spatial view of points
 
+
+library(sp)
+
+data(meuse)
+
+head(meuse)#to see the top value
+
+#make cooridnates
+coordinates(meuse) = ~x+y #we are using these coordinates
+
+plot(meuse)
+spplot(meuse, "zinc")
+
+#exercize: plot the spatial amount of copper
+spplot(muse, "copper", main="copper concentration")
+
+bubble(meuse, "zinc")
