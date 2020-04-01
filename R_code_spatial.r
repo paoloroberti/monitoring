f1ad00926a0aa192dc3532a73e9b21882e47c14c
paoloroberti <62661886+paoloroberti@users.noreply.github.com>
2020-04-01 17:19:29 +0200

    Update R_code_spatial.r

--- R_code_spatial.r
+++ R_code_spatial.r
@@ -14,6 +14,27 @@ plot(meuse)
 spplot(meuse, "zinc")
 
 #exercize: plot the spatial amount of copper
-spplot(muse, "copper", main="copper concentration")
+spplot(muse, "copper", main="Copper concentration")
 
 bubble(meuse, "zinc")
+bubble(meuse, "copper", main="Zinc concentration")
+
+#make it red coll=red
+
+
+
+
+###importing new data
+
+#download covi_agg.csv and put the fil into folder lab in C
+
+#setting the working directory: lab
+#windows
+setwd("C:/lab/")
+
+covid <- read.table("covid_agg.csv", head=T) #link an object to a function
+
+
+
+
+
