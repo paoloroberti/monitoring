f8f93870e0f1acd1aaa621def4d1d07dc9bc8374
Paolo Roberti <62661886+paoloroberti@users.noreply.github.com>
2020-04-15 17:43:12 +0200

    lesson 15/04

--- R_code_multivar.r
+++ R_code_multivar.r
@@ -1,1 +1,20 @@
+# R code for multivariate analysis
 
+setwd("C:/lab/")
+
+library(vegan)
+
+biomes<- read.table('biomes.csv', head=T, sep=',' )
+
+head(biomes)
+
+#DEtrended CORrespondence ANAlysis (decorana)--> put the data in two dimension
+multivar<- decorana (biomes)
+plot(multivar)
+
+head(biomes_types)
+
+attach(biomes_types)
+ordiellipse(multivar, type, col=1:4, kind="ehull", lwd=3)
+
+ordispider(multivar, type, col=1:4, label=T)
