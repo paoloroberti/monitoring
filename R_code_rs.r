f5ea03cdeb80d0603a3fa2efdda88674099cb340
Paolo Roberti <62661886+paoloroberti@users.noreply.github.com>
2020-04-24 18:05:16 +0200

    new

--- R_code_rs.r
+++ R_code_rs.r
@@ -59,4 +59,58 @@ plotRGB(p224r63_2011, r=3, g=2, b=4, stretch='Lin')
 ################
 setwd("C:/lab/")
 load("remote_sensing.RData")
-ls() 
+ls()
+
+library(raster)
+p224r63_1988<- brick("p224r63_1988_masked.grd") #importnig a compact set of diffetent bends(reflectance of different pixels in different wavelength)
+plot(p224r63_1988)
+
+#bands of Landsat
+#B1: blue
+#B2: green
+#B3: red
+#B4: NIR
+
+#Exercize: plot in visible RGB 321 both images
+par(mfrow=c(2,1))
+plotRGB(p224r63_1988, r=3, g=2, b=1, stretch='Lin')
+plotRGB(p224r63_2011, r=3, g=2, b=1, stretch='Lin')
+
+#Exercize: show the same plot in fals color with 432 RGB both images
+par(mfrow=c(2,1))
+plotRGB(p224r63_1988, r=4, g=3, b=2, stretch='Lin')
+plotRGB(p224r63_2011, r=4, g=3, b=2, stretch='Lin')
+
+#enhance the noise!
+par(mfrow=c(2,1))
+plotRGB(p224r63_1988, r=4, g=3, b=2, stretch='hist')
+plotRGB(p224r63_2011, r=4, g=3, b=2, stretch='hist')
+
+#DVI= NIR - RED. Difference vegetation index
+#bands of Landsat
+#B1: blue
+#B2: green
+#B3: red: B3_sre
+#B4: NIR: B4_sre
+
+dvi2011 <- p224r63_2011$B4_sre - p224r63_2011$B3_sre
+cl <- colorRampPalette(c('darkorchid3','light blue','lightpink4'))(100)
+plot(dvi2011,col=cl)
+
+#Exercize: dvi for 1988
+dvi1988 <- p224r63_1988$B4_sre - p224r63_1988$B3_sre
+cl <- colorRampPalette(c('darkorchid3','light blue','lightpink4'))(100)
+plot(dvi1988,col=cl)
+
+Difference between teh two year
+diff<- dvi2011 - dvi1988
+plot(diff)
+
+#changing the grain (the resolution basically)
+p224r63_2011res <- aggregate(p224r63_2011, fact=10) #fact is the amount of time we want to increase the dimesion of the pixel
+p224r63_2011res100 <- aggregate(p224r63_2011, fact=100) #fact is the amount of time we want to increase the dimesion of the pixel
+
+par(mfrow=c(3,1))
+plotRGB(p224r63_2011, r=4, g=3, b=2, stretch='Lin')
+plotRGB(p224r63_2011res, r=4, g=3, b=2, stretch='Lin')
+plotRGB(p224r63_2011res100, r=4, g=3, b=2, stretch='Lin')
