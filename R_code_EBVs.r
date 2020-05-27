93d47a0868320aeae9d737554c738db9cff69bfb
Paolo Roberti <62661886+paoloroberti@users.noreply.github.com>
2020-05-27 20:23:36 +0200

    new

--- R_code_EBVs.r
+++ R_code_EBVs.r
@@ -22,7 +22,7 @@ plotRGB(snt, 4, 3, 2, stretch = "lin" ) #NIR on top of red band. vegetaion is re
 pairs(snt) #to look at the relation between the bends
 #PC1 is ok to measure standard deviation
 
-##PCA analysis
+##PCA (principal component analysis) analysis of the component the better represent the sample
 sntpca <- rasterPCA(snt)
 sntpca
 
@@ -43,3 +43,47 @@ plot(sd_snt, col=cl)
 par(mfrow=c(1,2))
 plotRGB(snt, 4, 3, 2, stretch = "lin", main ="original imagine" ) #NIR on top of red band. vegetaion is red
 plot(sd_snt, col=cl, main= "diversity")
+
+### FOCAL on Cladonia
+
+setwd("C:/lab/")
+
+library(raster)
+
+#raster function import one layer per time while brick import more than one layer
+clad <- brick("cladonia_stellaris_calaita.JPG")
+plotRGB(clad, 1,2,3, stretch="lin")
+
+#measure variability and selection of the PC
+#first we select the extection of the window to pass on top of the image
+window <- matrix(1, nrow = 3, ncol = 3)
+window
+
+#PCA analysis
+library(RStoolbox) #for PCA
+
+cladpca <- rasterPCA(clad)
+cladpca
+
+summary(cladpca$model) #98% of image info is conatined in the first component
+
+plotRGB(cladpca$map, 1, 2, 3, stretch='Lin')
+
+
+#focal function applied to PC1 to identify the standard deviation
+sd_clad <- focal(cladpca$map$PC1, w=window, fun=sd)
+
+PC1_agg <- aggregate(cladpca$map$PC1, fact=10) #to acelerate the calculation
+sd_clad_agg <- focal(PC1_agg, w=window, fun=sd)
+
+cl <- colorRampPalette(c('yellow','violet','black'))(100)
+par(mfrow = c(1, 2))
+plot(sd_clad, col=cl)
+plot(sd_clad_agg, col=cl)
+
+# plot the calculation
+par(mfrow=c(1,2))
+cl <- colorRampPalette(c('yellow','violet','black'))(100) #
+plotRGB(clad, 1,2,3, stretch="lin")
+plot(sd_clad, col=cl)
+# plot(sd_clad_agg, col=cl)
